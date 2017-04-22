3b7035c82449a0ba7f516f752ef6de1aebdb0a9b
fedorath <kurtax.h1@googlemail.com>
2017-04-22 20:42:18 +0100

    Update r.py

--- r.py
+++ r.py
@@ -1,44 +1,50 @@
-import os
+##########################################################################################################
+##########################################################################################################
+###################################   Kurtis Hall 14019369 Project   #####################################
+##########################################################################################################
+##########################################################################################################
 
+#Multiple imports
+import os
 from SimpleCV import *
-# import shutil
 import shutil
-#Import smtplib, for the sending function
 import smtplib
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+##########################################################################################################
+#				Sending Attached PNG files to recipient.			         #
+##########################################################################################################
+
 def email(Gmail):
 
 	img_data = open(Gmail, 'rb').read()
         msg = MIMEMultipart('mixed')
-        msg['Subject'] = 'Important Message!'
+        msg['Subject'] = 'Important Message!'#subject title
         msg['From'] = 'kurtax.h1@googlemail.com'
         msg['Reply-to'] = ', '.join('kurtax.h1@googlemail.com')
-        text = MIMEText("Intruder has been spotted!")
-        msg.attach(text)
-
-    # open up an image file and attach it to the message
-
-        
+        text = MIMEText("Intruder has been spotted!")#email body text
+        msg.attach(text)#attach body text
         image = MIMEImage(img_data, name=os.path.basename(Gmail))
-        msg.attach(image)
+        msg.attach(image)#attaches img
 
-    # open up the SMTP server, start a tls connection, login, send, and close
-
-        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s = smtplib.SMTP('smtp.gmail.com', 587)#SMTP server connection
         s.ehlo()
-        s.starttls()
-        s.ehlo
-        s.login('kurtax.h1@googlemail.com', 'kurtax%1')
-        s.sendmail('kurtax.h1@googlemail.com','kurtax.h1@googlemail.com', msg.as_string())
-        s.close()
+        s.starttls()#Starts transport layer security
+        s.ehlo#Extended hello command
+        s.login('kurtax.h1@googlemail.com', 'kurtax%1')#Login Details
+        s.sendmail('kurtax.h1@googlemail.com','kurtax.h1@googlemail.com', msg.as_string())#Sending email
+        s.close()#Closes
+	
+##########################################################################################################
+#					SimpleCV Object detection.				         #
+##########################################################################################################
 
-#initialize the camer
-IMG = Camera()
-#set the max display size
-display = Display((800,600))
+IMG = Camera()#Camera is intiated.
+
+width = 640
+height = 480
 
 #create a threshold variable to change  motion sensitivity
 threshold = 5.0
@@ -124,4 +130,6 @@ while True:
 		original.save("pic/motion%s-%s.png" % (timestr, i))
 		#print results to terminal
 		print("Motion Detected")
-
+##########################################################################################################
+#						The END!					         #
+##########################################################################################################
