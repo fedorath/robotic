a1acc9db03787f4df317867184bf520cddcc6b52
fedorath <kurtax.h1@googlemail.com>
2017-04-22 21:37:17 +0100

    Update r.py

--- r.py
+++ r.py
@@ -11,6 +11,7 @@ from SimpleCV import *
 import shutil
 import smtplib
 import numpy as np
+import uuid
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -42,95 +43,89 @@ def email(Gmail):
 ##########################################################################################################
 #					SimpleCV Object detection.				         #
 ##########################################################################################################
-Time = 10
+
 
 IMG = Camera()#Camera is intiated.
 
 width = 640
 height = 480
-threshold = 10
-#create a threshold variable to change  motion sensitivity
+threshold = 10#Threshold set to 
+Time = 10#Time it takes to send the email
 
-#set timer variables for email loop
 Stime = time.time()
 
+dst = "pic" 
+bkp = "pic_bkp" 
 
-#create destination & backup directories for the pictures
-dst = "pic" #destination directory for images
-bkp = "pic_bkp" #backup  directory for images
 
-#if the picture directories don't exist, create them
 if not os.path.exists("pic"):
 	os.makedirs("pic")
 if not os.path.exists("pic_bkp"):
 	os.makedirs("pic_bkp")
 
-#create a loop that constantly grabs new images from the webcam
-while True:
-        #set a time variable that updates with the loop
+	
+while True:#While loop which grabs images until it is told to stop.
+
         settime = time.time()
-        #grab an image still from the camera and convert it to grayscale
+
         img01 = IMG.getImage().toGray()
-        #wait half a second
+
         time.sleep(0.5)
-	#grab an unedited still to use as our original image
+
 	original = IMG.getImage()
-        #grab another image still from the camera and conver it to grayscale
+
         img02 = IMG.getImage().toGray()
-        #subract the images from each other, binarize and inver the colors
+
         diff = (img01 - img02).binarize(50).invert()
 
-        #dump all the values into a Numpy matrix and extract the mean avg
+
         matrix = diff.getNumpy()
         mean = matrix.mean()
 
-	#find and highlight the objects within the image
+
 	blobs = diff.findBlobs()
 
-        #check to see if the wait time has been passed
-	#if current_time >= (start_time + wait_time):
-		#if it has, reset the start time
-
 	if settime >= (Stime + Time):
-		#scan the picture directory for files
+
 		for root, dirs, files in os.walk(dst):
 			dst_root = root.replace(dst, bkp)
-			#if a file is found in the picture directory, send it to email
+
 			if files:
 				firstfile = sorted(files)[0]
 				mailer = os.path.join(root, firstfile)
 				email(mailer)
-				#move any files in the pic directory to the backup directory
+
 			for file_ in files:
 				src_file = os.path.join(root, file_)
 				dst_file = os.path.join(dst_root, file_)
 				shutil.move(src_file, dst_root)
 
-        #if the mean is greater than our threshold variable, then look for objects
 	if mean >= threshold:
 
-		#check to see if any objects were detected
+
 		if blobs:
-			#find the central point of each object
-			#and draw a red circle around it
+
 			for b in blobs:
 				try:
-					loc = (b.x,b.y) #locates center of object
+					loc = (b.x,b.y) 
 					original.drawCircle(loc,b.radius(),Color.RED,2)
 				except:
 					e = sys.exc_info()[0]
+					
+					
+					
 		#use the current date to create a unique file name
 		timestr = time.strftime("%Y%m%d-%H%M%S")
-		
+		filestr = str(uuid.uuid4())
 		#initialize the counter variable
 		i = 1
 		
 		#check to see if the filename already exists
-		while os.path.exists("pic/motion%s-%s.png" % (timestr, i)):
+		while os.path.exists(uuid.uuid4(filestr, i)):
 			#if it does, add one to the filename and try again
 			i += 1
 		#once a unique filename has been found, save the image
-		original.save("pic/motion%s-%s.png" % (timestr, i))
+		original.save(uuid.uuid4(filestr, i))
 		#print results to terminal
 		print("Motion Detected")
 ##########################################################################################################
