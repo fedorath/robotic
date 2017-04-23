008b27b72d906d4373e1dabcf8b3bb21a5a3caf0
fedorath <kurtax.h1@googlemail.com>
2017-04-23 13:29:03 +0100

    Create av.py

---
+++ av.py
@@ -0,0 +1,31 @@
+import pi2go, time
+
+pi2go.init()
+
+# Here we set the speed to 40 out of 100 - feel free to change!
+speed = 40
+
+# Here is the main body of the program - a lot of while loops and ifs!
+# In order to get your head around it go through the logical steps slowly!
+try:
+  while True:
+    if pi2go.irLeft():
+      while pi2go.irLeft():
+        # While the left sensor detects something - spin right
+        pi2go.spinRight(speed)
+      pi2go.stop()
+    if pi2go.irRight():
+      while pi2go.irRight():
+        # While the right sensor detects something - spin left
+        pi2go.spinLeft(speed)
+      pi2go.stop()
+    while not (pi2go.irLeft() or pi2go.irRight()):
+      if pi2go.getDistance() <= 0.3: # If the distance is less than 0.3, spin right for 1 second
+        pi2go.spinRight(speed)
+        time.sleep(1)
+      else:
+        pi2go.forward(speed)
+    pi2go.stop()
+
+finally: # Even if there was an error, cleanup
+  pi2go.cleanup()
