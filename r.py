717b75ca8394d1ead6347b14a48f9afcf0538ffa
fedorath <kurtax.h1@googlemail.com>
2017-04-24 18:49:13 +0100

    Update r.py

--- r.py
+++ r.py
@@ -58,10 +58,12 @@ while True:#While loop which grabs images until it is told to stop.
 	
         PIC1 = IMG.getImage().toGray()
 
-        time.sleep(0.5)
+        time.sleep(1)
 
 	PIC = IMG.getImage()
 
+	time.sleep(2)
+	
         PIC2 = IMG.getImage().toGray()
 	
         d = (PIC1 - PIC2).binarize().invert()
